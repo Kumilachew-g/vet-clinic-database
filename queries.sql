49824e030de855d75d36f9993e6b881533f06183
kumilachew-g <kumilachew1212@gmail.com>
2022-10-19 16:51:37 +0300

    Update transaction animals table

--- queries.sql
+++ queries.sql
@@ -3,6 +3,7 @@
 SELECT * FROM animals WHERE name LIKE '%mon';
 
 SELECT name FROM animals WHERE date_of_birth BETWEEN '01-01-2016' AND '01-01-2019';
+
 SELECT name FROM animals WHERE neutered =true  AND  escape_attempts <3;
 
 SELECT date_of_birth
@@ -14,3 +15,26 @@ SELECT * FROM animals WHERE neutered =true;
 
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+/* -- Transaction update the animals table -- */
+BEGIN;
+UPDATE animals SET species ='unspecified';  --made update on species column
+SELECT species FROM animals; --verify the change was made
+ROLLBACK;
+SELECT species FROM animals; --verify roll back the change
+
+/*-- Update animals table and Commit --*/ 
+
+BEGIN;
+UPDATE animals SET species ='digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species ='pokemon' WHERE species IS NULL;
+SELECT name,species from animals; --verify update
+COMMIT;
+SELECT name,species from animals; --verify update made after commit
+
+/*-- Delete all animals data inside transaction and undo deletion --*/
+BEGIN;
+DELETE FROM animals;
+SELECT COUNT(*) FROM animals; --check the count of animals 
+SELECT * FROM animals; -- check after deletion
+ROLLBACK;
