20a12face109c650e734ec9381039e9dd2f7ed7e
kumilachew-g <kumilachew1212@gmail.com>
2022-10-19 14:06:42 +0300

    Write animals table queries

--- queries.sql
+++ queries.sql
@@ -1,2 +1,16 @@
 /* Populate database with sample data. */
 
+SELECT * FROM animals WHERE name LIKE '%mon';
+
+SELECT name FROM animals WHERE date_of_birth BETWEEN '01-01-2016' AND '01-01-2019';
+SELECT name FROM animals WHERE neutered =true  AND  escape_attempts <3;
+
+SELECT date_of_birth
+FROM animals WHERE name ='Agumon' OR name ='Pikachu';
+
+SELECT name, escape_attempts from animals WHERE weight_kg >10.5
+
+SELECT * FROM animals WHERE neutered =true;
+
+SELECT * FROM animals WHERE name <> 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
