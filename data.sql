096f7569ea5b32ec586114dcfbb29cdee2b1ef61
kumilachew-g <kumilachew1212@gmail.com>
2022-10-19 14:05:23 +0300

    Insert data

---
+++ data.sql
@@ -0,0 +1,13 @@
+/* Populate database with sample data. */
+
+INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts,neutered) VALUES 
+('Agumon', '03-02-2020', 10.23, 0,true);
+
+INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts,neutered) VALUES 
+('Gabumon', '11-15-2018', 8, 2,true);
+
+INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts,neutered) VALUES 
+('Pikachu', '7-01-2021', 15.04, 1,false);
+
+INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts,neutered) VALUES 
+('Davimon', '12-05-2017', 11, 2,true);
