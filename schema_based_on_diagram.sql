65de7f1f4c44e47cc071d12d3c2a70f3dbeb8808
kumilachew-g <kumilachew1212@gmail.com>
2022-10-25 17:57:32 +0300

    Add patients table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,1 +1,8 @@
+-- Create database from the diagram
 CREATE DATABASE medical_clinic;
+
+CREATE TABLE patients (
+	ID INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	NAME VARCHAR(255),
+	date_of_birth date
+);
